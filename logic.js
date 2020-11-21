1adb98ea185592b352e6d5c0f86a147ef8d29206
Zion Brown <zabs95@gmail.com>
2020-11-21 17:35:54 -0500

    Added basic combat to the constructer

--- logic.js
+++ logic.js
@@ -3,18 +3,35 @@ console.log("working")
 
 console.log(document.getElementsByTagName("body"))
 
-function Entity(type, name, condition, guard) {
+function Entity(type, name, condition, guard, brawn) {
 this.type = type;
 this.name = name;
 this.condition = condition;
 this.guard = guard;
+this.brawn = brawn
 
 this.printStats = function() {
-    console.log("Type: " + this.type + "\nName: " +
-    this.name + "\nContition: " + this.condition + "\nGuard: " + this.guard)
-}
+    console.log("Type: " + this.type + "\nName: " + this.name +
+     "\nContition: " + this.condition + "\nGuard: " + this.guard + 
+     "\nBrawn: " + this.brawn)
 }
 
-var player = new Entity("Player", "Human", 100, 100);
+this.attack = function(foe) {
+    foe.condition -= this.brawn;
+    console.log(this.type + " attacks " + foe.name + " for " + this.brawn + " damage.")
+    console.log(foe.name + " has " + foe.condition + " health left")
+};
+
+}
+
+var player = new Entity("Player", "Human", 100, 100, 10);
+var skeleton = new Entity("Enemy", "Skeleton", 50, 50, 5);
 
 player.printStats();
+skeleton.printStats();
+
+player.attack(skeleton);
+player.attack(skeleton);
+player.attack(skeleton);
+
+skeleton.printStats();
